2a55f7c701bfc3e71c81a78c88410c750c96c573
subramaniajeeva <subramaniajeeva@gmail.com>
2013-03-24 20:25:44 +0530

    module mongo added. It'll accept Generic model as data for inserting, updating/saving. Find will now return a list of result in genmodel object form. FindById will return a single genmodel object.

--- mongoBase.py
+++ mongoBase.py
@@ -1,6 +1,6 @@
 from pymongo import MongoClient
 from bson.objectid import ObjectId
-import genmodel
+from genmodel import GenericModel
 
 class MongoBase :
 
@@ -12,7 +12,7 @@ class MongoBase :
 		self._db = connection[_dbName]
 
 	def insert(self, data, collection) :
-		self._db[collection].insert(collection.asDict())
+		self._db[collection].insert(data.asDict())
 	
 	def remove(self, pattern, collection) :
 		self._db[collection].remove(pattern)
@@ -20,25 +20,40 @@ class MongoBase :
 	def removeById(self, _id, collection) :
 		self._db[collection].remove({"_id" :  ObjectId(_id)})
 	
-	def update(self, pattern, data, collection) :
-		self._db[collection].update(pattern, data)
+	def update(self, pattern, data, collection, option = 'set', _upsertStatus = False) :
+		_data = data.asDict()
+		self._db[collection].update(pattern, { '$' + option : _data}, upsert = _upsertStatus)
 	
-	def save(self, pattern, data, collection) :
-		self._db[collection].update(pattern, data, upsert=True)
+	def save(self, pattern, data, collection, option = 'set') :
+		self.update(pattern, data, collection, option, True)
 	
 	def find(self, pattern, collection) :
-		return self._db[collection].find(pattern)
+		resultSet = self._db[collection].find(pattern)
+		return [self._processResult(document) for document in resultSet]
+	
+	def _processResult(self, result) :
+		result['id'] = str(result['_id'])
+		del result['_id']
+		print type(result)
+		return GenericModel(result)
 	
 	def findById(self, _id, collection) :
-		return self._db[collection].findOne({"_id": ObjectId(_id)})
+		result = self._db[collection].find_one({"_id": ObjectId(_id)})
+		return self._processResult(result)
 
 def getConf() :
+	# mocking getconfig 
 	return None
 
 #db = MongoBase("pymongo", "localhost", 27017)
-#db.insert({"name":"jeeva"}, "user")
+#db.insert(GenericModel({"name":"jeeva"}), "user")
 #print db.removeById("514dbe2a4443cf2a78438beb", "user")
 #print [user for user in db.find({"name":"jeeva"}, "user")]
-#db.update({"name":"jeeva"}, {'$set' :{"age": 23} }, "user")
-#db.save({"name":"arpit"},{'$set' :{"age":22}}, "user")
+#db.update({"name":"jeeva"}, GenericModel({"age": 23}), "user")
+#db.save({"name":"arpit"},GenericModel({"age":23}), "user")
 #print [user for user in db.find({"name":"jeeva"}, "user")]
+#print 'Hello' == u'Hello'
+#x = db.findById("514ea3664443cf4cd93b2897","user")
+#print x.getName()
+#y = db.find({"name":"arpit"}, 'user')
+#print y[0].getName()
