cc0272db1c6230276461ccfc5bd6493d227c8fa3
thefourtheye <thechargingvolcano@gmail.com>
2013-04-06 12:01:00 +0530

    Added docstring to Config Module

--- Config.py
+++ Config.py
@@ -1,7 +1,29 @@
+"""
+	Helper module that retrieves configurations from the file
+"""
+
 from ConfigParser import SafeConfigParser
 from genericmodel import GenericModel
 
 def GetConfigurations(CfgFileName):
+	"""
+	Returns a GenericModel filled with the configuration settings, read from the file `CfgFileName`.
+	If there is a problem, this would return an empty GenericModel object. An example for the configuration 
+	settings is as follows
+
+		[DB]
+		host = localhost
+		port = 12345 ;Be sure to convert to an int
+		db   = test
+
+	Here, DB is the section name and the configuration names are host, port and db where as the
+	configuration values are localhost, 12345, test respectively. The text that follows semi-colon (;)
+	will be ignored. So to get the host value,
+
+		config = GetConfigurations ('/var/lib/MyCfg.cfg')
+		print config.getDB().getHost()
+
+	"""
 	Configurations = GenericModel()
 	try:
 		parser = SafeConfigParser()
