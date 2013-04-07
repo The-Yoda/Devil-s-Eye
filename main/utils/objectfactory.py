616eff7215a148ab4842f08444cb1935822fbb1b
subramaniajeeva <subramaniajeeva@gmail.com>
2013-04-08 00:19:15 +0530

    Got objectfactory working. Need to make it return singleton instance

--- main/utils/objectfactory.py
+++ main/utils/objectfactory.py
@@ -1,8 +1,28 @@
-import imp
 import sys
+import os
 from configmanager import ConfigManager
-
+from main import rootAbsDir
+from main import rootDir
 class Factory(type) :
+
+	def findModule(self, moduleName, dirName):
+		files = os.listdir(dirName)
+		for element in files:
+			file = os.path.abspath(dirName) + os.sep + element
+			if os.path.isdir(file) and os.path.exists(file + os.sep + "__init__.py") :
+				modulePath, isFound = self.findModule(moduleName, file)
+				if isFound == True :
+					return modulePath, True 
+			elif element[:-3] == moduleName :
+				return file , True
+		return None, False
+
+	def loadModule(self, rootAbs, moduleName) :
+		path, isFound = self.findModule(moduleName.lower(), rootAbs)
+		if isFound == False : 
+			raise Exception('No such module exists')
+		moduleName = (rootDir + path[len(rootAbsDir):-3]).replace('/','.')
+		return __import__(moduleName, fromlist = ["*"])
 
 	def __getattr__(self, method):
 		try:
@@ -12,9 +32,7 @@ class Factory(type) :
 				def default(*args, **kwargs):
 					conf = getattr(ConfigManager.getMainConfig(), method)()
 					moduleName = method[3:] if conf is None else conf  
-					fp, pathName, description = imp.find_module('main.lib.db.' + moduleName.lower())
-					print ConfigManager.getMainConfig()
-					module = imp.load_module(moduleName, fp, pathName, description)
+					module = self.loadModule(rootAbsDir, moduleName)	
 					return getattr(module, moduleName)()
 				return default
 		except ImportError:
@@ -23,13 +41,6 @@ class Factory(type) :
 class ObjectFactory:
     __metaclass__ = Factory 
 
-def getConf(module, confName):
-	if confName == 'Db' :
-		return "MongoDb"
-	"""
-	Mocking configmanager which will be implemented later
-	"""
-	return None
 """
 This class ObjectFactory can be used to improve dynamicity of modules.
 Ex :  If a user needs an instance of a class (whose name is specified in config), 
@@ -43,7 +54,7 @@ Ex :  If a user needs an instance of a class (whose name is specified in config)
 	Usage :
 	"""
 myclass1 = ObjectFactory.getDb() 
-print myclass1.findById("514ea3664443cf4cd93b2897", 'user').getName()
+print myclass1.findById("514ea3664443cf4cd93b2897", 'user')
 myclass2 = ObjectFactory.getMongoDb()
 print myclass2.findById("514ea3664443cf4cd93b2897", 'user').getName()
 
